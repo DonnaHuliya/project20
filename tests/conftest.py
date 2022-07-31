25e4de4ea24185a32abcaabb1e6720c103d13aa5
Дарья <bykhovets_da@mail.ru>
2022-07-31 15:34:02 +0300

    update1

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ from unittest.mock import MagicMock
 import pytest
 
 from dao.director import DirectorDAO
+from dao.genre import GenreDAO
 from dao.model.director import Director
+from dao.model.genre import Genre
 
 
 @pytest.fixture()
@@ -15,8 +17,24 @@ def director_dao():
 
     director_dao.get_one = MagicMock(return_value=d1)
     director_dao.get_all = MagicMock(return_value=[d1, d2])
-    director_dao.create = MagicMock(d3)
+    director_dao.create = MagicMock(return_value=d3)
     director_dao.update = MagicMock()
     director_dao.delete = MagicMock()
 
     return director_dao
+
+
+@pytest.fixture()
+def director_dao():
+    genre_dao = GenreDAO(None)
+    comedy = Genre(id=1, name="Comedy")
+    drama = Genre(id=2, name="Drama")
+    horror = Genre(id=3, name="Horror")
+
+    genre_dao.get_one = MagicMock(return_value=comedy)
+    genre_dao.get_all = MagicMock(return_value=[comedy, drama])
+    genre_dao.create = MagicMock(return_value=horror)
+    genre_dao.update = MagicMock()
+    genre_dao.delete = MagicMock()
+
+    return genre_dao
--- tests/services/test_director.py
+++ tests/services/test_director.py
@@ -1,6 +1,6 @@
 import pytest
 
-from demostration_solution.service.director import DirectorService
+from service.director import DirectorService
 
 
 class TestDirectorService:
@@ -9,5 +9,26 @@ class TestDirectorService:
         self.director_service = DirectorService(dao=director_dao)
 
     def test_get_one(self):
-        director = self.director_service.get_one()
+        director = self.director_service.get_one(1)
         assert director is not None
+        assert director.id is not None
+
+    def test_get_all(self):
+        directors = self.director_service.get_all()
+        assert len(directors) > 0
+
+    def test_create(self):
+        director = {'name': 'New Name'}
+        new_director = self.director_service.create(director)
+        assert new_director is not None
+
+    def test_update(self):
+        director_d = {
+            'id': 3,
+            "name" = "Leonid",
+        }
+        self.director_service.update(director_d)
+
+    def test_delete(self):
+        res = self.director_service.delete(1)
+        assert res is None
